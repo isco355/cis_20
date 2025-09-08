99aba505f91bc1ea1fab62692d85e6529cb79292
francisco <ortega.fo53@gmail.com>
2025-09-08 12:19:35 -0700

    hw3.8

--- assigments/hw_3_8/task2/main.s
+++ assigments/hw_3_8/task2/main.s
@@ -1,6 +1,6 @@
-# Program Name: printName&Age 
+# Program Name: printThreeParameters
 # Author: Francisco Ortega Pantoja
-# Date: 9/07/2020 
+# Date: 9/08/2020 
 
 .text 
 .global main 
@@ -10,38 +10,17 @@ main:
     SUB sp, sp, #4
     STR lr, [sp, #0] 
     
-    # Prompt for string input 
-    LDR r0, =string_prompt
-    BL printf 
-    # Scanf (request user enter a string)
-    LDR r0, =str_input 
-    LDR r1, =str1
-    BL scanf 
-
-
-    # Prompt for string input 
-    LDR r0, =string_prompt
-    BL printf 
-    # Scanf (request user enter a string)
-    LDR r0, =str_input 
-    LDR r1, =str2
-    BL scanf 
-
-
-    
-    # Prompt for string input 
-    LDR r0, =prompt_age
-    BL printf 
-    # Scanf (request user age)
-    LDR r0, =age_input 
-    LDR r1, =age 
-    BL scanf 
-    
+     
     # Printing the message 
     LDR r0, =format 
-    LDR r1, =name 
-    LDR r2, =age 
-    LDR r2,[r2]
+    LDR r1, =num1 
+    LDR r2, =num2
+    LDR r3, =num3
+    MOV r1, #5
+    MOV r2, #7
+    MOV r3, #9
+
+    # LDR r2,[r2]
     BL printf 
     
     # Return to the OS 
@@ -50,18 +29,8 @@ main:
     MOV pc, lr 
     
 .data 
-    # Prompt string message
-    string_prompt: .asciz "Enter a string:" 
-    # Format for string input (read a string) 
-    str_input: .asciz "%s" 
-    # Reserves space in the memory for name 
-    str1: .space 40
-    str2: .space 40
-    # Prompt number message
-    int_prompt: .asciz "Enter a number: " 
-    # Format for int input (read a int) 
-    int_input: .asciz "%d" 
-    # Reserves space in the memory for number 
-    number: .word 0
+    num1: .word 12
+    num2: .word 2
+    num3: .word 53
     # Format of the program output
-    format: .asciz "int1 = %d, int2 = %d, and int3 = %d" 
+    format: .asciz "int1 = %d, int2 = %d, and int3 = %d \n" 
